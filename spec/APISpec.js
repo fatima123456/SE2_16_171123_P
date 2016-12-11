c3068f592ce767029177a720dddc6f5fdbdf89fd
saghir <you@example.com>
2016-12-11 20:53:08 +0100

    add spec for apis

---
+++ spec/APISpec.js
@@ -0,0 +1,51 @@
+//test of the APIs
+
+//lib for sending request
+var request = require("request");
+
+/* Test for /
+  * it checks if the server answers with 200 code header
+  */
+describe("Test /", function() {
+    it("returns status code 200", function(done) {
+        request.get('http://localhost:1337/', 
+            function(error, response, body) {
+                expect(response.statusCode).toBe(200);
+                done();
+            });
+    }); 
+});
+
+/* Test for /ita
+  * it checks if the server answers with 200 code header
+  */
+describe("Test /ita", function() {
+    it("returns status code 200", function(done) {
+        request.get('http://localhost:1337/ita', 
+            function(error, response, body) {
+                expect(response.statusCode).toBe(200);
+                done();
+            });
+    }); 
+});
+
+//library for JSON requests
+requestJSON = require('request-json');
+var client = requestJSON.createClient("http://localhost:1337/");
+
+/* Test for /tab
+  * it checks if the server answers with 200 code header
+  */
+describe("Test /tab", function() {
+    var data = {city:'Trento', depart:'average'};
+    it("returns status code 200", function(done) {
+        client.post('http://localhost:1337/tab',data, 
+            function(error, response, body) {
+                expect(response.statusCode).toBe(200);
+                done();
+            });
+    }); 
+});
+
+
+
